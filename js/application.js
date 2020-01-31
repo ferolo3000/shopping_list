3f1dc1650612d99f2c1d6c2d32114c8568c64308
Fernanda <fernandaromero85@gmail.com>
2020-01-30 22:21:11 -0500

    delete function added

--- js/application.js
+++ js/application.js
@@ -1,20 +1,49 @@
 $(document).ready(function() {
 
-  $('button').click(function() {
+  $('#btn').click(function() {
     var itemInput = $('#item').val();
-		var priceInput = $('#price').val();
-		var qtyInput = $('#qty').val();
-		var total = priceInput * qtyInput;
-    $('.add').append('<li class="item-name col-xs-3">' + itemInput + '</li>');
-		$('.add').append('<li class="item-name col-xs-3">' + "$" + priceInput + '</li>');
-		$('.add').append('<li class="item-name col-xs-3">' + qtyInput + '</li>');
-		$('.add').append('<li class="item-name col-xs-2">' + "$" + total + '<li id="delete" class="delete delete-item item-name col-xs-1"><a class="link-delete">X</a>' + '</li>');
+    var priceInput = $('#price').val();
+    var qtyInput = $('#qty').val();
+    var totalItem = priceInput * qtyInput;
+    $('#list-container').append('<div class="add row item-list"><li class="item-name col-xs-3">' + itemInput + '</li>' +
+	'<li class="item-value col-xs-3">' + "$" + priceInput + '</li>' + '<li class="item-qty col-xs-3">' + qtyInput + '</li>' +
+'<li  id="total" class="item-cost col-xs-2">' + "$" + totalItem + '<li id="delete" class="delete delete-item item-name col-xs-1"><a class="link-delete">X</a>' + '</li></div>');
 
     $(document).on('click', '.delete', deleteItem);
   });
+
+  // var test = $('#total').map(function () {
+  // 			return Number($(this).text().replace(/\$/,""));
+  // 		}).get();
+
+
+  $('input').keydown(function(e) {
+    // console.log(e.which);
+    if (e.which == 13) {
+      sum();
+    }
+  });
+
+	var total = 0;
+
+	var sum = function() {
+
+	  var prices = $('.item-value');
+	  var qtys = $('.item-qty');
+	  total = 0;
+
+	  for (i = 0; i < qtys.length; i++) {
+
+	    var price = Number($(prices[i]).text().replace(/\$/, ""));
+	    var subtotal = (Number($(qtys[i]).val())) * price;
+	       total += subtotal;
+	  }
+	  $('#total-price').text(total);
+	  return total;
+	};
 });
 
 //delete item from list
 function deleteItem() {
-  $(this).parent().remove();
+  $(this).parents(".row").remove();
 }
